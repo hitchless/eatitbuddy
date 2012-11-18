8e5f27fc4c06e9c725f3460700f29dc1501886d7
Jonny Miller <jonny@hitchless.com>
2012-11-17 21:50:02 -0800

    initial commit of eat it buddy app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eatitbuddy::Application.config.secret_token = 'f2c30b67c38f5d4e7f9c5832d8d157774ef49d04ca6be946dd433913faf2f88f41612857679c281032efac1e56e9fa20c73733bcf95ffc813691c896d062a1cf'
